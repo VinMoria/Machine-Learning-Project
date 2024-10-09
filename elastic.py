679221efb45f6589ed3d9877a6242cb32b749c28
Lexie99 <lexi_he99@outlook.com>
2024-10-09 17:05:49 +0800

    elastic

---
+++ elastic.py
@@ -0,0 +1,42 @@
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+
+#feature normalization
+scaler = MinMaxScaler()
+X_train, X_test, y_train, y_test = train_test_split(X_crime, y_crime,
+                                                   random_state = 0)
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#train Ridge model
+linridge = Ridge(alpha=20.0).fit(X_train_scaled, y_train)
+#find best alpha for ridge
+print('Ridge regression: effect of alpha regularization parameter\n')
+for this_alpha in [0, 1, 10, 20, 50, 100, 1000]:
+    linridge = Ridge(alpha = this_alpha).fit(X_train_scaled, y_train)
+    r2_train = linridge.score(X_train_scaled, y_train)
+    r2_test = linridge.score(X_test_scaled, y_test)
+    num_coeff_bigger = np.sum(abs(linridge.coef_) > 1.0)
+    print('Alpha = {:.2f}\nnum abs(coeff) > 1.0: {}, \
+r-squared training: {:.2f}, r-squared test: {:.2f}\n'
+         .format(this_alpha, num_coeff_bigger, r2_train, r2_test))
+
+
+#train Lasso model
+linlasso = Lasso(alpha=2.0, max_iter = 10000).fit(X_train_scaled, y_train)
+#print effective feature for lasso
+for e in sorted (list(zip(list(X_crime), linlasso.coef_)),
+                key = lambda e: -abs(e[1])):
+    if e[1] != 0:
+        print('\t{}, {:.3f}'.format(e[0], e[1]))
+print('Lasso regression: effect of alpha regularization\n\
+parameter on number of features kept in final model\n')
+#find best alpha for lasso
+for alpha in [0.5, 1, 2, 3, 5, 10, 20, 50]:
+    linlasso = Lasso(alpha, max_iter = 10000).fit(X_train_scaled, y_train)
+    r2_train = linlasso.score(X_train_scaled, y_train)
+    r2_test = linlasso.score(X_test_scaled, y_test)
+    print('Alpha = {:.2f}\nFeatures kept: {}, r-squared training: {:.2f}, \
+r-squared test: {:.2f}\n')
